6642c935038c958fe62da89e107949493f78c625
gqq <jilianggqq@163.com>
2014-12-13 03:06:04 +0800

    ditu

---
+++ GasApp/src/com/bjgas/gasapp/ImgTest.java
@@ -0,0 +1,17 @@
+package com.bjgas.gasapp;
+
+import com.bjgas.common.BaseActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class ImgTest extends BaseActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_img_test);
+	}
+}
